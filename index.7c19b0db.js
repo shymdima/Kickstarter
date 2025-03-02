b86521419ce730979e18c9e4c703131bca9d0e8f
dima <shymdima74@gmail.com>
2025-03-02 19:58:24 +0200

    make build

---
+++ index.7c19b0db.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.querySelector(".main__form");e&&e.addEventListener("submit",function(t){t.preventDefault(),e.reset()})});
+//# sourceMappingURL=index.7c19b0db.js.map
